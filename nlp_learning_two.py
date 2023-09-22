eba07408b0bf101b334c25ffd9dcca8219b55a2e
joseline25 <youjoseline@gmail.com>
2023-09-22 12:24:34 +0100

    nlp testing

---
+++ nlp_learning_two.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.text import Tokenizer # tokenizer API
+
+sentences = [
+    'I love my dog',
+    'I love my cat'
+]
+
+tokenizer = Tokenizer(num_words=100)  # max numbers of words to keep
+tokenizer.fit_on_texts(sentences)
+word_index = tokenizer.word_index
+
+print(word_index)
